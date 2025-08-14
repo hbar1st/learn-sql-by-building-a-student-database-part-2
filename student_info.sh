7ee5a92c3ea5d3373ffb927d0926d4e246595cc8
hbar1st <35541470+hbar1st@users.noreply.github.com>
2025-08-14 11:47:44 -0400

    save at around 30% done

---
+++ student_info.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Info about my computer science students from students database
+
+echo -e "\n~~ My Computer Science Students ~~\n"
+
+PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+
+echo -e "\nFirst name, last name, and GPA of students with a 4.0 GPA:"
+
+echo "$($PSQL "select first_name, last_name, gpa from students where gpa = 4.0")"
+
+echo -e "\nAll course names whose first letter is before 'D' in the alphabet:"
+
+echo "$($PSQL "select course from courses where course < 'D'")"
+
+echo -e "\nFirst name, last name, and GPA of students whose last name begins with an 'R' or after and have a GPA greater than 3.8 or less than 2.0:"
+
+echo "$($PSQL "select first_name, last_name, gpa from students where last_name >= 'R' and (gpa > 3.8 or gpa < 2.0)")"
+
+echo -e "\nLast name of students whose last name contains a case insensitive 'sa' or have an 'r' as the second to last letter:"
+
+echo "$($PSQL "select last_name from students where last_name ilike '%sa%' or last_name like '%r_'")"
+
+echo -e "\nFirst name, last name, and GPA of students who have not selected a major and either their first name begins with 'D' or they have a GPA greater than 3.0:"
+
+echo "$($PSQL "select first_name, last_name, gpa from students where major_id is null and (first_name like 'D%' or gpa > 3.0)")"
+
+echo -e "\nCourse name of the first five courses, in reverse alphabetical order, that have an 'e' as the second letter or end with an 's':"
+
+echo "$($PSQL "select course from courses where course like '_e%' or course like '%s' order by course desc limit 5")"
+
+echo -e "\nAverage GPA of all students rounded to two decimal places:"
