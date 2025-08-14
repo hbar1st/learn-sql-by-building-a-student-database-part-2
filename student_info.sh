bb3368afe35bbe5ff2e0f4f31f4fe542bdbf14f6
hbar1st <35541470+hbar1st@users.noreply.github.com>
2025-08-14 12:33:38 -0400

    Save progress: 1960.1

--- student_info.sh
+++ student_info.sh
@@ -33,3 +33,5 @@ echo "$($PSQL "select course from courses where course like '_e%' or course like
 echo -e "\nAverage GPA of all students rounded to two decimal places:"
 
 echo "$($PSQL "select round(avg(gpa),2) from students")"
+
+echo -e "\nMajor ID, total number of students in a column named 'number_of_students', and average GPA rounded to two decimal places in a column name 'average_gpa', for each major ID in the students table having a student count greater than 1:"
